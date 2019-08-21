26ac1e4e15982c704b90a02dc64e5a12e7822f39
Ale de la Vina <alejandro.delavina@gruposantander.com>
2019-08-21 10:45:11 +0200

    New framework

--- FBAnnotationClusteringSwift/FBAnnotationClusteringSwift/FBAnnotationClusterTemplate.swift
+++ FBAnnotationClusteringSwift/FBAnnotationClusteringSwift/FBAnnotationClusterTemplate.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 public enum FBAnnotationClusterDisplayMode {
 	case SolidColor(sideLength: CGFloat, color: UIColor)
--- FBAnnotationClusteringSwift/FBAnnotationClusteringSwiftTests/FBAnnotationClusteringSwiftTests.swift
+++ FBAnnotationClusteringSwift/FBAnnotationClusteringSwiftTests/FBAnnotationClusteringSwiftTests.swift
@@ -1,29 +1,34 @@
-import UIKit
-import XCTest
-@testable import FBAnnotationClustering
+//
+//  FBAnnotationClusteringSwiftTests.swift
+//  FBAnnotationClusteringSwiftTests
+//
+//  Created by DE LA VIÑA - ALEJANDRO MARTIN on 21/08/2019.
+//  Copyright © 2019 com.adelavina. All rights reserved.
+//
 
-class Tests: XCTestCase {
-    
+import XCTest
+@testable import FBAnnotationClusteringSwift
+
+class FBAnnotationClusteringSwiftTests: XCTestCase {
+
     override func setUp() {
-        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure() {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
